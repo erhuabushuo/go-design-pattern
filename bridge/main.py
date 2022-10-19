7e7b875cbf5c3841ec22c2f84db6d626dbe5f02d
Aidan.He <erhuabushuo@gmail.com>
2022-10-19 11:50:41 +0800

    add bridge and composite

---
+++ bridge/main.py
@@ -0,0 +1,54 @@
+from abc import ABC, abstractmethod
+
+
+class Implementation(ABC):
+    @abstractmethod
+    def operation_implementation(self) -> str:
+        pass
+
+
+class ConcreteImplementationA(Implementation):
+    def operation_implementation(self) -> str:
+        return "ConcreteImplementationA: Here's the result on the platform A."
+
+
+class ConcreteImplementationB(Implementation):
+    def operation_implementation(self) -> str:
+        return "ConcreteImplementationB: Here's the result on the platform B."
+
+
+class Abstraction:
+    implementation: Implementation
+
+    def __init__(self, implementation: Implementation) -> None:
+        self.implementation = implementation
+
+    def operation(self) -> str:
+        return (
+            f"Abstraction: Base operation with:\n"
+            f"{self.implementation.operation_implementation()}"
+        )
+
+
+class ExtendedAbstraction(Abstraction):
+    def operation(self) -> str:
+        return (
+            f"ExtendedAbstraction: Extended operation with:\n"
+            f"{self.implementation.operation_implementation()}"
+        )
+
+
+def client_code(abstraction: Abstraction) -> None:
+    print(abstraction.operation())
+
+
+if __name__ == "__main__":
+    implementation = ConcreteImplementationA()
+    abstraction = Abstraction(implementation)
+    client_code(abstraction)
+
+    print("\n")
+
+    implementation = ConcreteImplementationB()
+    abstraction = ExtendedAbstraction(implementation)
+    client_code(abstraction)
