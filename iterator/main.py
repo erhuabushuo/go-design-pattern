f9912c12276f7e0f5670d979c24a377173f3979d
Aidan.He <erhuabushuo@gmail.com>
2022-10-25 20:21:15 +0800

    add iterator and mediator

---
+++ iterator/main.py
@@ -0,0 +1,56 @@
+from __future__ import annotations
+from collections.abc import Iterable, Iterator
+from typing import Any, List
+
+
+class WordsCollection(Iterable):
+    _collection: List[Any] = None
+
+    def __init__(self, collection: List[str] = None) -> None:
+        self._collection = [] if collection is None else collection
+
+    def __iter__(self) -> AlphabeticalOrderIterator:
+        return AlphabeticalOrderIterator(self._collection)
+
+    def get_reverse_iterator(self) -> AlphabeticalOrderIterator:
+        return AlphabeticalOrderIterator(self._collection, True)
+
+    def add_item(self, item: str):
+        self._collection.append(item)
+
+
+class AlphabeticalOrderIterator(Iterator):
+    _position: int = None
+    _reverse: bool = False
+    _collection: WordsCollection = None
+
+    def __init__(self, collection: WordsCollection, reverse: bool = False) -> None:
+        self._collection = collection
+        self._reverse = reverse
+        self._position = -1 if reverse else 0
+
+    def __next__(self) -> str:
+        try:
+            value = self._collection[self._position]
+            self._position += -1 if self._reverse else 1
+        except IndexError:
+            raise StopIteration()
+
+        return value
+
+
+if __name__ == "__main__":
+    # The client code may or may not know about the Concrete Iterator or
+    # Collection classes, depending on the level of indirection you want to keep
+    # in your program.
+    collection = WordsCollection()
+    collection.add_item("First")
+    collection.add_item("Second")
+    collection.add_item("Third")
+
+    print("Straight traversal:")
+    print("\n".join(collection))
+    print("")
+
+    print("Reverse traversal:")
+    print("\n".join(collection.get_reverse_iterator()))
