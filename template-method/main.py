af6ccc30b5be0f2145773c2bf81123bf185eb09c
Aidan.He <erhuabushuo@gmail.com>
2022-10-27 16:25:01 +0800

    add template-method and vistor patterns

---
+++ template-method/main.py
@@ -0,0 +1,85 @@
+from abc import ABC, abstractmethod
+
+
+class AbstractClass(ABC):
+    def template_method(self) -> None:
+        self.base_operation1()
+        self.required_operations1()
+        self.base_operation2()
+        self.hook1()
+        self.required_operations2()
+        self.base_operation3()
+        self.hook2()
+
+    def base_operation1(self) -> None:
+        print("AbstractClass says: I am doing the bulk of the work")
+
+    def base_operation2(self) -> None:
+        print("AbstractClass says: But I let subclasses override some operations")
+
+    def base_operation3(self) -> None:
+        print("AbstractClass says: But I am doing the bulk of the work anyway")
+
+    @abstractmethod
+    def required_operations1(self) -> None:
+        raise NotImplementedError()
+
+    @abstractmethod
+    def required_operations2(self) -> None:
+        raise NotImplementedError()
+
+    def hook1(self) -> None:
+        pass
+
+    def hook2(self) -> None:
+        pass
+
+
+class ConcreteClass1(AbstractClass):
+    """
+    Concrete classes have to implement all abstract operations of the base
+    class. They can also override some operations with a default implementation.
+    """
+
+    def required_operations1(self) -> None:
+        print("ConcreteClass1 says: Implemented Operation1")
+
+    def required_operations2(self) -> None:
+        print("ConcreteClass1 says: Implemented Operation2")
+
+
+class ConcreteClass2(AbstractClass):
+    """
+    Usually, concrete classes override only a fraction of base class'
+    operations.
+    """
+
+    def required_operations1(self) -> None:
+        print("ConcreteClass2 says: Implemented Operation1")
+
+    def required_operations2(self) -> None:
+        print("ConcreteClass2 says: Implemented Operation2")
+
+    def hook1(self) -> None:
+        print("ConcreteClass2 says: Overridden Hook1")
+
+
+def client_code(abstract_class: AbstractClass) -> None:
+    """
+    The client code calls the template method to execute the algorithm. Client
+    code does not have to know the concrete class of an object it works with, as
+    long as it works with objects through the interface of their base class.
+    """
+
+    # ...
+    abstract_class.template_method()
+    # ...
+
+
+if __name__ == "__main__":
+    print("Same client code can work with different subclasses:")
+    client_code(ConcreteClass1())
+    print("")
+
+    print("Same client code can work with different subclasses:")
+    client_code(ConcreteClass2())
